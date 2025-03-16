fb728d8deadae05619d7fbc52e68ae300b19878d
joevgreathead <joevgreathead@gmail.com>
2025-03-16 00:27:12 -0400

    Extend job submission page and add flash - Update job runner page to use a dynamic list of job types - Add flash to main layout with support and color for both alert and notice - Add first i18n values for flash messages

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,10 +1,22 @@
 # frozen_string_literal: true
 
 class TestController < ApplicationController
+  include TestHelper
+
   def index; end
 
   def create
-    TimeJob.perform_async(params[:input])
+    job_type = params[:job_type]
+
+    unless valid_type?(job_type)
+      flash.alert = I18n.t('errors.invalid_job_type')
+
+      redirect_to test_index_url
+      return
+    end
+
+    job_type.constantize.perform_async(params[:input])
+    flash.notice = I18n.t('notice.job_type_queued', job_type:)
 
     redirect_to test_index_url
   end
--- app/helpers/test_helper.rb
+++ app/helpers/test_helper.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
 module TestHelper
+  JOB_TYPES = [
+    TimeJob
+  ].freeze
+
+  def job_options
+    JOB_TYPES.map do |value|
+      [value.to_s, value.to_s]
+    end
+  end
+
+  def valid_type?(type)
+    result = JOB_TYPES.any? { |job_type| job_type.to_s == type }
+    puts "Looking for [#{type}] Result: #{result}"
+
+    result
+  end
 end
